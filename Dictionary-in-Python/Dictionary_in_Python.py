f39bb73d162159f6bcb6e253007899dba5dfdd15
ashori2002 <mohammadashori2002@gmail.com>
2023-02-07 00:21:54 +0330

    get

--- Dictionary-in-Python/Dictionary_in_Python.py
+++ Dictionary-in-Python/Dictionary_in_Python.py
@@ -1,6 +1,9 @@
 
 mydict = {"red":"ghermez","green":"sabz","blue":"abi"}
 print(mydict["green"])
+#_______________________________________________________
 del mydict["red"]
 print(mydict)
+#_______________________________________________________
+print(mydict.get("gree","====ERROR===="))
 
